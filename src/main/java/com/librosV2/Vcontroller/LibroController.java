22fd8461b7bba2d4816897b9f2498d365a9ef36d
nicoherrerabdsol <jherrera@bdso.com.ar>
2024-04-19 11:00:57 -0300

    Elimino imports

--- src/main/java/com/librosV2/Vcontroller/LibroController.java
+++ src/main/java/com/librosV2/Vcontroller/LibroController.java
@@ -1,30 +1,23 @@
 package com.librosV2.Vcontroller;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.librosV2.Wservice.LibroService;
+import com.librosV2.Zmodel.Libro;
+import com.librosV2.util.ValidarNumero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.librosV2.Wservice.LibroService;
-import com.librosV2.Zmodel.Libro;
-import com.librosV2.util.ValidarNumero;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 
